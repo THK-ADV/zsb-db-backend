1a8b67248df63c42fd39d602c0ae37eba3319491
Philipp Schmeier <philipp.schmeier@gmail.com>
2020-03-10 16:06:15 +0100

    Refactoring

--- src/main/kotlin/api/SchuleApi.kt
+++ src/main/kotlin/api/SchuleApi.kt
@@ -2,17 +2,15 @@ package api
 
 import dto.SchuleDto
 import io.ktor.application.call
-import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.request.receive
-import io.ktor.response.respondText
 import io.ktor.routing.*
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonConfiguration
 import kotlinx.serialization.list
 import service.SchuleService
 
-fun Route.schulenApi() {
+fun Route.schuleApi() {
     val serializer = Json(JsonConfiguration.Stable)
 
     route("schulen") {
@@ -32,26 +30,20 @@ fun Route.schulenApi() {
             call.respondJsonOk(json)
         }
 
-        put {
-            call.logRequest()
-            val schuleDto = call.receive<SchuleDto>()
-            if (schuleDto.schule_id == null) {
-                call.respondText("ID not given. Use post to create.", ContentType.Text.Plain, HttpStatusCode.BadRequest)
-                return@put
-            }
-            val result = SchuleService.createOrUpdate(schuleDto)
-            call.respond(HttpServerResponse.map(result))
-        }
-
         post {
             call.logRequest()
             val schuleDto = call.receive<SchuleDto>()
-            if (schuleDto.schule_id != null) {
-                call.respondText("ID given. Use put to update instead.", ContentType.Text.Plain, HttpStatusCode.BadRequest)
-                return@post
-            }
+            if (call.checkIdAndRespondUsePutIfNotNull(schuleDto.schule_id)) return@post
             val result = SchuleService.createOrUpdate(schuleDto)
             call.respond(HttpServerResponse.map(result, HttpStatusCode.Created))
+        }
+
+        put {
+            call.logRequest()
+            val schuleDto = call.receive<SchuleDto>()
+            if (call.checkIdAndRespondUsePostIfNull(schuleDto.schule_id)) return@put
+            val result = SchuleService.createOrUpdate(schuleDto)
+            call.respond(HttpServerResponse.map(result))
         }
     }
 }
--- src/main/kotlin/api/apiUtil.kt
+++ src/main/kotlin/api/apiUtil.kt
@@ -28,3 +28,25 @@ suspend fun ApplicationCall.respondJsonOk(json: JsonElement) =
 suspend fun ApplicationCall.respond(response: HttpServerResponse) {
     this.respondText(response.text, response.type, response.status)
 }
+
+/**
+ * @return true if the id is not given and a respond was send
+ */
+suspend fun ApplicationCall.checkIdAndRespondUsePostIfNull(id: Int?): Boolean {
+    if (id == null) {
+        this.respondText("ID not given. Use post to create.", ContentType.Text.Plain, HttpStatusCode.BadRequest)
+        return true
+    }
+    return false
+}
+
+/**
+ * @return true if the id is given and a respond was send
+ */
+suspend fun ApplicationCall.checkIdAndRespondUsePutIfNotNull(id: Int?): Boolean {
+    if (id != null) {
+        this.respondText("ID not given. Use post to create.", ContentType.Text.Plain, HttpStatusCode.BadRequest)
+        return true
+    }
+    return false
+}
