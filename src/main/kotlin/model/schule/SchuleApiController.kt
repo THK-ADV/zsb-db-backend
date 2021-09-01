4921f5833513b9da585c092fc3be12e0da591480
AnleAnja <anleanja@live.de>
2021-09-01 11:38:24 +0200

    rename school dto

--- src/main/kotlin/model/schule/SchuleApiController.kt
+++ src/main/kotlin/model/schule/SchuleApiController.kt
@@ -49,7 +49,7 @@ fun Route.schulenApi() = route("schools") {
     post {
         call.logRequest()
         val schuleDto = call.receive<SchuleDto>()
-        if (call.checkIdAndRespondUsePutIfNotNull(schuleDto.schule_id)) return@post
+        if (call.checkIdAndRespondUsePutIfNotNull(schuleDto.school_id)) return@post
         val result = SchuleDao.createOrUpdate(schuleDto)
         call.respond(HttpServerResponse.map(result, HttpStatusCode.Created))
     }
@@ -57,7 +57,7 @@ fun Route.schulenApi() = route("schools") {
     put {
         call.logRequest()
         val schuleDto = call.receive<SchuleDto>()
-        if (call.checkIdAndRespondUsePostIfNull(schuleDto.schule_id)) return@put
+        if (call.checkIdAndRespondUsePostIfNull(schuleDto.school_id)) return@put
         val result = SchuleDao.createOrUpdate(schuleDto)
         call.respond(HttpServerResponse.map(result))
     }
