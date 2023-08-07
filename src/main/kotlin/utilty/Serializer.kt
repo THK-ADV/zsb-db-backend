94b9c311aee6f287c29baf7c3f42631c7974f685
Anja Bertels <anja.bertels@th-koeln.de>
2023-08-07 11:50:29 +0200

    Delete Serializer.kt

--- src/main/kotlin/utilty/Serializer.kt
+++
@@ -1,9 +0,0 @@
-package utilty
-
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
-
-/*
-object Serializer {
-    val stable = Json(JsonConfiguration.Stable)
-}*/
